a3abd097a870d17f472146696174df9b1f361104
BlueOceanWave <97416032+BlueOceanWave@users.noreply.github.com>
2022-06-13 20:20:18 -0500

    Initial commit

---
+++ Teaching/src/Lect01.java
@@ -0,0 +1,22 @@
+public class Lect01 {
+
+    public static void main (String args[]){
+
+        // This is a comment
+        // This is another comment
+        // This is a third comment
+
+        /*
+        
+        This is a multiline comment
+        We can have multiple comments
+
+        */
+        
+        System.out.println("He said: \\Hello there\""); // An inline comment
+        System.out.print("Hello");
+        
+
+    }
+}
+
