62a1af5605ed88af196715ccb9c9f68b674a2ff9
BlueOceanWave <97416032+BlueOceanWave@users.noreply.github.com>
2022-07-06 22:28:15 -0500

    Finished Lect06

--- Classes/Person.java
+++ Classes/Person.java
@@ -4,21 +4,36 @@ package Classes;
 public class Person {
 
     //Class Data
-    private String firstName, lastName;
-    private int age;
+    private String firstName, lastName, dob;
+    private int age, ID;
+
+    private static int count = 0;
 
     //Constructor
-    public Person (String userFirstName, String userLastName, int userAge) {
-        firstName = userFirstName;
-        lastName = userLastName;
+    public Person (String firstName, String lastName, int age) {
+       this(firstName, lastName, age, "0/0/0");
+    }
+
+    public Person (String firstName, String lastName, int age, String dob){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = dob;
+        this.ID = count;
 
         //Perform a check on the age
-        if (userAge<0) {
-            age = 0;
+        if (age<0) {
+            this.age = 0;
         }
         else {
-            age = userAge;
+            this.age = age;
         }
+
+        count++;
+
+    }
+
+    public static void greeting(){
+        System.out.println("Hello, I am a person " + count);
     }
 
 
@@ -46,6 +61,28 @@ public class Person {
     
         age = newAge;
 
+    }
+
+    public String getDOB(){
+        return dob;
+    }
+
+
+    private String upperCaseName(){
+        return firstName.toUpperCase() + " " + lastName.toUpperCase();
+    }
+
+    public boolean equal(Person p){
+        boolean isEqual = (this.firstName == p.firstName) &&
+                          (this.lastName == p.lastName) &&
+                          (this.age == p.age) &&
+                          (this.dob == p.dob);
+
+        return isEqual;
+    }
+
+    public String toString(){
+        return upperCaseName() + ", " + age;
     }
 
 }
